a88df9e9f2631013e3ce447d2e8ff2b51e31cbff
SadBarin <SadBarin@yandex.by>
2020-05-24 19:45:41 +0300

    Начал делать навигацию для mobile

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,20 @@
+// 'use strict';
+
+// let burger  = document.getElementById('header__menu');
+// let nav     = document.getElementById('header__nav');
+// let navList = document.getElementById('header__list');
+
+// burger.onclick = function() {
+// 	burger.style.display = 'none';
+// 	navList.style.display = 'block';
+// 	nav.style.flexDirection = 'column';
+// }
+
 document.body.onload = function preloader() {
 	setTimeout(function() {
-		var preloader = document.getElementById('page-preloader');
+		let preloader = document.getElementById('page-preloader');
 		if(!preloader.classList.contains('done')) {
 			preloader.classList.add('done');
 		}
 	}, 100)
-}
+};

f8193975d8df0cadd514f34e6cb1f771ea963577
SadBarin <SadBarin@yandex.by>
2020-05-30 17:25:30 +0300

    Адаптировал навигацию

--- src/script/script.js
+++ src/script/script.js
@@ -1,20 +1,31 @@
-// 'use strict';
-
-// let burger  = document.getElementById('header__menu');
-// let nav     = document.getElementById('header__nav');
-// let navList = document.getElementById('header__list');
-
-// burger.onclick = function() {
-// 	burger.style.display = 'none';
-// 	navList.style.display = 'block';
-// 	nav.style.flexDirection = 'column';
-// }
+'use strict';
 
 document.body.onload = function preloader() {
 	setTimeout(function() {
-		let preloader = document.getElementById('page-preloader');
+		let preloader = document.getElementById('preloader');
 		if(!preloader.classList.contains('done')) {
 			preloader.classList.add('done');
 		}
 	}, 100)
 };
+
+let burger    = document.getElementById('header__menu'),
+    nav       = document.getElementById('header__nav'),
+    navList   = document.getElementById('header__list'),
+    closeNav  = document.getElementById('header__list-close');
+
+burger.onclick = () => {
+	header__nav.classList.add('overlay');
+	burger.style.display = 'none';
+	navList.style.display = 'block';
+	closeNav.style.display = 'block';
+	nav.style.flexDirection = 'column';
+};
+
+closeNav.onclick = () => {
+	header__nav.classList.remove('overlay');
+	closeNav.style.display = 'none';
+	navList.style.display = 'none';
+	nav.style.flexDirection = 'row';
+	burger.style.display = 'block';
+}
